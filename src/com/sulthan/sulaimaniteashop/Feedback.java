f4bbecd51c3e264ec0612cf6b21d201dae4bb746
kousthubraja <kousthubraja@gmail.com>
2015-06-30 17:54:58 +0530

    Upload all

---
+++ src/com/sulthan/sulaimaniteashop/Feedback.java
@@ -0,0 +1,46 @@
+package com.sulthan.sulaimaniteashop;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+
+public class Feedback extends Activity {
+
+	EditText txtfeedback;
+	
+	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feedback);
+        
+        txtfeedback = (EditText) findViewById(R.id.txtfeedback);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.feedback, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    
+    public void submitFeedback(View v){
+    	txtfeedback.setText("sent");
+    }
+}
